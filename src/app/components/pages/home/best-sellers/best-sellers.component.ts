7a2ebaa98580d0071be52e0a8224e763189a2659
yvan.kombou <yvan.kombou@kaeyros-analytics.com>
2022-01-28 09:44:31 +0100

    first commit

---
+++ src/app/components/pages/home/best-sellers/best-sellers.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { ShopService } from 'src/app/components/services/shop.service';
+
+@Component({
+  selector: 'app-best-sellers',
+  templateUrl: './best-sellers.component.html',
+  styleUrls: ['./best-sellers.component.css']
+})
+export class BestSellersComponent extends ShopService {
+  @Input() title = ''
+}
